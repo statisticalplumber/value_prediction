c32181e360bfef722c5f53630625910f5b3f0571
statisticalplumber <furianpandit@gmail.com>
2018-06-27 23:45:34 +0530

    latest commit

---
+++ src/exploratory_analysis.R
@@ -0,0 +1,7 @@
+# exploratory analysis of value prediction files to begin
+
+library(data.table)
+
+# reading train input file
+
+df_train = fread('../resource/train.csv')
---
+++ src/exploratory_analysis.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 22:24:11 2018
+
+@author: kr
+"""
+import pandas as pd
+
+# exploratory analysis of value_prediction datasets
+
+df_train = pd.read_csv('/')
